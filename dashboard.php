15d77bdd8b91d15963d51a6fac0bbf79926cacf2
Clarestabertha <clarestabertha8@gmail.com>
2023-11-15 14:32:17 +0700

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,43 @@
+<!--konten-->
+<br><br><br><p class="kal1">HOT MAKEUP</p><br>
+<!--HOT MAKEUP-->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="coba.webp" class="d-block w-100" alt="jennie">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="maybeline.webp" class="d-block w-100" alt="karina">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="baren.webp" class="d-block w-100" alt="somi">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <p class="kal1">TOP REVIEW</p>
+    <div class="ulasan">
+        <div class="product">
+            <div class="image-container">
+                <img src="coba.webp">
+            </div>
+            <h5>Skintific Cushion</h5>
+        </div>
+        <div class="deskripsi">
+            <p>Beli ini karena banyak yang rekomen. Ini pertama kalinya aku pakai cushion jadi kurang tau cushion lain performanya gimana tapi ini cushion bagus banget, sih. Sekali tepuk udah bisa nutupin bekas jerawat atau kalau kulit lagi kemerahan. Ada efek glow-nya di wajah jadi ada kesan dewy gitu. Shade-nya pas pula di aku. Bakal repurchase lagi sih kalo habis ❤️</p>
+        </div>
+    </div>    
